d4d6f22bf6566333715e600278b1cb5b8cc32b30
Lugtastic <77817859+Lugtastic@users.noreply.github.com>
2021-08-24 12:05:16 -0500

    Update EcuX-V2.lua

--- EcuX-V2.lua
+++ EcuX-V2.lua
@@ -1,4 +1,12 @@
 if game.PlaceId == (5104202731) then
+local Notification = loadstring(game:HttpGet("https://api.irisapp.ca/Scripts/IrisBetterNotifications.lua"))()
+
+Notification.Notify("Ecu-X V2", "Southwest Florida", "rbxasset://textures/ui/GuiImagePlaceholder.png", {
+Duration = 2,       
+Main = {
+    Rounding = true,
+}
+});
 loadstring(game:HttpGet(('https://raw.githubusercontent.com/Lugtastic/hubs/main/EcuX-V2-SFW.lua'),true))()
 elseif game.PlaceId == (891852901) then
 loadstring(game:HttpGet(('https://raw.githubusercontent.com/Lugtastic/hubs/main/EcuX-V2-Greenville.lua'),true))()
