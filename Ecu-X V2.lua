35815917e1dc89b7199d7ee51b4700645273e1d9
Lugtastic <77817859+Lugtastic@users.noreply.github.com>
2021-08-24 11:14:17 -0500

    Create Ecu-X V2.lua

---
+++ Ecu-X V2.lua
@@ -0,0 +1,5 @@
+if game.PlaceId == (5104202731) then
+loadstring(game:HttpGet(('https://raw.githubusercontent.com/Lugtastic/hubs/main/EcuX-V2-SFW.lua'),true))()
+elseif game.PlaceId == (891852901) then
+loadstring(game:HttpGet(('https://raw.githubusercontent.com/Lugtastic/hubs/main/EcuX-V2-Greenville.lua'),true))()
+end
